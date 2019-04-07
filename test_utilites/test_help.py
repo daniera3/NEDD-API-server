69347731b2e5da2aa8490e8dff44b2a58a812b4f
dani <daniera3@ac.sce.ac.il>
2019-04-07 10:29:16 +0300

    jenkins

--- test_utilites/test_help.py
+++ test_utilites/test_help.py
@@ -10,6 +10,8 @@ def id_generator(size=7, chars=string.ascii_uppercase + string.digits):
 
 
 def register(client, username, password, type_of_user,Email):
+    username='admin'
+    password='admin'
     return client.post('/handle_data', data=dict(
         Register_New_User=username,
         Register_New_Password=password,
